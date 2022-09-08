061ef9c077c9191d34e65227148422c98730e39e
Axtitan <terence.m1801102@st.futminna.edu.ng>
2022-09-08 21:52:58 +0100

    quote c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - print a string to standard error
+*Description: using the main function\
+*This program prints to stderr
+*return 1
+*/
+int main(void)
+{
+char m[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fprintf(stderr, "%s", m);
+return (1);
+}
