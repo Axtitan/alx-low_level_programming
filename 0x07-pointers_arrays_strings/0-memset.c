291de8a69512e5943909bd4767b41e31c99d4ba4
Axtitan <79796852+Axtitan@users.noreply.github.com>
2022-09-26 19:43:01 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,11 @@
 #include "main.h"
 #include <string.h>
-
 /**
-* _memset -> this memory set function
-* @s: string
-* @b: a charcter
-* @n: an integer
-* Return:  a string
+*_memset -> this memory set function
+*@s: string
+*@b: a charcter
+*@n: an integer
+*Return:  a string
 */
 char *_memset(char *s, char b, unsigned int n)
 {
